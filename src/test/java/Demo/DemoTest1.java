9edee3b88fac2e0126473d23527221e186f5639d
suma <admin@DESKTOP-T9782A1>
2026-08-03 15:36:16 +0530

    selected all staged element

---
+++ src/test/java/Demo/DemoTest1.java
@@ -0,0 +1,16 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+	
+	@Test(groups="regression")
+	public void add()
+	{
+		System.out.println("add");
+	}
+
+	}
+
+
